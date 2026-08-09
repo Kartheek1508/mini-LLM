f26a5d8fd229110552bded642e156efb7fed1a6e
Kartheek1508 <bnskartheek15168@gmail.com>
2026-08-09 23:20:07 +0530

    added comments for better understanding

--- data_pipeline.py
+++ data_pipeline.py
@@ -10,7 +10,7 @@ import os
 from datasets import load_dataset
 
 def main():
-
+#loading dataset
     dataset = load_dataset(
         "HuggingFaceFW/fineweb-edu",
         name="sample-10BT",
@@ -18,11 +18,11 @@ def main():
         streaming=True
     )
 
-    filename = "train.bin"
+    filename = "train.bin"#file for the memmap
 
     with open(filename,"wb") as f:
         batch = []
-        batch_size = 32
+        batch_size = 32#doing in batches cause doing it all at once is inefficent
         for sample in dataset.take(1000):
             batch.append(sample["text"])
 
@@ -31,11 +31,11 @@ def main():
                 token_ids = []
                 for doc in docs:
                     token_ids.extend(tokenize(doc))
-                arr = np.asarray(token_ids,dtype=np.uint16)
+                arr = np.asarray(token_ids,dtype=np.uint16) #directly storing it to a binary file instead of using np.memmap()
                 arr.tofile(f)
                 batch = []
 
-        if batch:
+        if batch: #this entire if is just when there aren't batch_size docs left
                 docs = preprocess(batch)
                 token_ids = []
                 for doc in docs:
@@ -43,7 +43,7 @@ def main():
                 arr = np.asarray(token_ids,dtype=np.uint16)
                 arr.tofile(f)
 
-    tokens = np.memmap(
+    tokens = np.memmap( #just accesing the tokens we made above
         "train.bin",
         dtype=np.uint16,
         mode="r"
@@ -75,7 +75,7 @@ def main():
     loader = DataLoader(dataset,batch_size=8)
 
     batch = next(iter(loader))
-    print(batch.shape)
+    print(batch.shape)#should be (8,1024) cause __getitem__ only gives 1024 and 8 cause we set batch_size=8 
 
 
     sample = batch[0].tolist()
