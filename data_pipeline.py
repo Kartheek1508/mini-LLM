37d7b1fafc96f7b759421b9d8ee7fd286654e93d
Kartheek1508 <bnskartheek15168@gmail.com>
2026-08-30 13:04:39 +0530

    added better resuming and error handling

--- data_pipeline.py
+++ data_pipeline.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 import torch
 import os
+import json
 
 from datasets import load_dataset
 
@@ -25,7 +26,8 @@ class Token_dataset(Dataset):
         return len(self.data)
 
 def main():
-#loading dataset
+    TARGET_TOKENS = 1_000_000
+
     dataset = load_dataset(
         "HuggingFaceFW/fineweb-edu",
         name="sample-10BT",
@@ -33,59 +35,145 @@ def main():
         streaming=True
     )
 
-    filename = "train.bin"#file for the memmap
+    filename = "train.bin"
+    progress_file = "progress.json"
 
-    with open(filename,"wb") as f:
-        batch = []
-        batch_size = 32#doing in batches cause doing it all at once is inefficent
-        for sample in dataset.take(1000):
+    batch_size = 32
+    last_reported = 0
+
+    # Load previous progress
+    if os.path.exists(progress_file):
+        with open(progress_file, "r") as f:
+            progress = json.load(f)
+
+        total_tokens = progress["tokens"]
+        documents_processed = progress["documents"]
+        checkpoint_bytes = progress["file_bytes"]
+
+        print(
+            f"Resuming from {total_tokens:,} tokens "
+            f"and {documents_processed:,} documents"
+        )
+
+        # Remove anything after the last confirmed checkpoint
+        with open(filename, "r+b") as f:
+            f.truncate(checkpoint_bytes)
+
+        # Skip documents that were already processed
+        dataset = dataset.skip(documents_processed)
+
+    else:
+        total_tokens = 0
+        documents_processed = 0
+        checkpoint_bytes = 0
+
+        print("Starting from the beginning")
+
+    last_reported = total_tokens
+
+    batch = []
+
+    with open(filename, "ab") as f:
+
+        for sample in dataset:
+
             batch.append(sample["text"])
 
             if len(batch) == batch_size:
+
                 docs = preprocess(batch)
+
                 token_ids = []
+
                 for doc in docs:
                     token_ids.extend(tokenize(doc))
-                arr = np.asarray(token_ids,dtype=np.uint16) #directly storing it to a binary file instead of using np.memmap()
+
+                remaining = TARGET_TOKENS - total_tokens
+
+                if len(token_ids) > remaining:
+                    token_ids = token_ids[:remaining]
+
+                arr = np.asarray(token_ids, dtype=np.uint16)
                 arr.tofile(f)
+
+                total_tokens += len(token_ids)
+                documents_processed += len(batch)
+
                 batch = []
 
-        if batch: #this entire if is just when there aren't batch_size docs left
-                docs = preprocess(batch)
-                token_ids = []
-                for doc in docs:
-                    token_ids.extend(tokenize(doc))
-                arr = np.asarray(token_ids,dtype=np.uint16)
-                arr.tofile(f)
+                # Make sure the data is written before checkpointing
+                f.flush()
+                os.fsync(f.fileno())
 
-    tokens = np.memmap( #just accesing the tokens we made above
-        "train.bin",
-        dtype=np.uint16,
-        mode="r"
-    )
+                checkpoint_bytes = f.tell()
 
-    print("Total tokens:", len(tokens))
-    print("File size:", os.path.getsize("train.bin"), "bytes")
+                # Save checkpoint
+                temp_file = progress_file + ".tmp"
+
+                with open(temp_file, "w") as pf:
+                    json.dump({
+                        "tokens": total_tokens,
+                        "documents": documents_processed,
+                        "file_bytes": checkpoint_bytes
+                    }, pf)
+
+                    pf.flush()
+                    os.fsync(pf.fileno())
+
+                os.replace(temp_file, progress_file)
+
+                # Print every ~10M tokens
+                if total_tokens - last_reported >= 10_000_000:
+                    print(f"Tokens: {total_tokens:,} / {TARGET_TOKENS:,}")
+                    last_reported = total_tokens
+
+                if total_tokens >= TARGET_TOKENS:
+                    break
+
+        # Process remaining documents
+        if batch and total_tokens < TARGET_TOKENS:
+
+            docs = preprocess(batch)
+
+            token_ids = []
+
+            for doc in docs:
+                token_ids.extend(tokenize(doc))
+
+            remaining = TARGET_TOKENS - total_tokens
+            token_ids = token_ids[:remaining]
+
+            arr = np.asarray(token_ids, dtype=np.uint16)
+            arr.tofile(f)
+
+            total_tokens += len(token_ids)
+            documents_processed += len(batch)
+
+            f.flush()
+            os.fsync(f.fileno())
+
+            checkpoint_bytes = f.tell()
+
+            temp_file = progress_file + ".tmp"
+
+            with open(temp_file, "w") as pf:
+                json.dump({
+                    "tokens": total_tokens,
+                    "documents": documents_processed,
+                    "file_bytes": checkpoint_bytes
+                }, pf)
+
+                pf.flush()
+                os.fsync(pf.fileno())
+
+            os.replace(temp_file, progress_file)
+
+    print(f"Total tokens: {total_tokens:,}")
+    print("File size:", os.path.getsize(filename), "bytes")
 
 
-    data = np.memmap("train.bin",dtype=np.uint16,mode="r")
-
-
-    data = np.memmap("train.bin",dtype=np.uint16,mode="r")
-
-    dataset = Token_dataset(data)
-
-    loader = DataLoader(dataset,batch_size=8)
-
-    batch = next(iter(loader))
-    print(batch.shape)#should be (8,1024) cause __getitem__ only gives 1024 and 8 cause we set batch_size=8 
-
-
-    sample = batch[0].tolist()
-
-    text = decode(sample)
-    print(text)
-
+if __name__ == "__main__":
+    main()
 
 if __name__ =="__main__":
     main()
